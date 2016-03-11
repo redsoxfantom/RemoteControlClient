1f538e3c624bbbf1c5cbf0786d1d76a8fa6bbfd9
Tom <redsoxfantom@gmail.com>
2016-03-10 20:42:47 -0500

    initial commit

---
+++ app/src/main/java/com/csc8570/remotecontrolclient/networking/Data.java
@@ -0,0 +1,14 @@
+package com.csc8570.remotecontrolclient.networking;
+
+/**
+ * Created by Tom on 3/9/2016.
+ */
+public class Data
+{
+    @lombok.Data
+    public static class BeaconPacket
+    {
+        private String ipAddress;
+        private String friendlyName;
+    }
+}
