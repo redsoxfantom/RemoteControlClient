3465b8934a16f6ff08018308bed5acb501f90b03
Tom <redsoxfantom@gmail.com>
2016-03-14 17:29:50 -0400

    Added button when beacon listener finds a server

--- app/src/main/java/com/csc8570/remotecontrolclient/SelectServerActivity.java
+++ app/src/main/java/com/csc8570/remotecontrolclient/SelectServerActivity.java
@@ -1,19 +1,28 @@
 package com.csc8570.remotecontrolclient;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.csc8570.remotecontrolclient.Interfaces.IBeaconReceiver;
 import com.csc8570.remotecontrolclient.networking.BeaconListener;
 
+import java.util.ArrayList;
+
 public class SelectServerActivity extends AppCompatActivity implements IBeaconReceiver {
 
     BeaconListener listener;
     Button serverSearchButton;
-    TextView foundServersBox;
+    ProgressBar serverSearchBar;
+    LinearLayout serverButtonsLayout;
     boolean searchForServers;
 
     @Override
@@ -24,7 +33,8 @@ public class SelectServerActivity extends AppCompatActivity implements IBeaconRe
 
         listener = new BeaconListener(this);
         serverSearchButton = (Button)findViewById(R.id.ServerSearchButton);
-        foundServersBox = (TextView)findViewById(R.id.FoundServersBox);
+        serverSearchBar = (ProgressBar)findViewById(R.id.ServerSearchProgressBar);
+        serverButtonsLayout = (LinearLayout)findViewById(R.id.FoundServersLayout);
     }
 
     // Called when the user requests a search for active servers
@@ -34,12 +44,14 @@ public class SelectServerActivity extends AppCompatActivity implements IBeaconRe
         {
             listener.startListening();
             serverSearchButton.setText("Stop Searching For Servers");
+            serverSearchBar.setVisibility(View.VISIBLE);
             searchForServers = false;
         }
         else
         {
             listener.stopListening();
             serverSearchButton.setText("Search For Servers");
+            serverSearchBar.setVisibility(View.INVISIBLE);
             searchForServers = true;
         }
     }
@@ -47,17 +59,41 @@ public class SelectServerActivity extends AppCompatActivity implements IBeaconRe
     @Override
     public void addServer(final String ipAddress, final String friendlyName)
     {
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                foundServersBox.append("FriendlyName "+friendlyName+", IPAddress "+ipAddress);
-            }
-        });
+        //this.runOnUiThread(new FoundServerHandler(this.getApplicationContext(),friendlyName,ipAddress));
+        this.runOnUiThread(new HandleAddServer(friendlyName,ipAddress,getApplicationContext()));
     }
 
     @Override
     public void removeServer(String ipAddress, String friendlyName)
     {
 
+    }
+
+    // Handles a new server being added to the layout
+    private class HandleAddServer implements Runnable
+    {
+        private String friendlyName;
+        private String ipAddress;
+        private Context ctx;
+        private LayoutInflater inflater;
+
+        public HandleAddServer(String friendlyName, String ipAddress, Context ctx)
+        {
+            this.friendlyName = friendlyName;
+            this.ipAddress = ipAddress;
+            this.ctx = ctx;
+            inflater = LayoutInflater.from(ctx);
+        }
+
+        @Override
+        public void run()
+        {
+            LinearLayout foundServerBtn = (LinearLayout)inflater.inflate(R.layout.server_search_result,null);
+            TextView friendlyNameText = (TextView) foundServerBtn.findViewById(R.id.ServerFriendlyName);
+            TextView ipAddressText = (TextView) foundServerBtn.findViewById(R.id.ServerIPAddress);
+            friendlyNameText.setText(friendlyName);
+            ipAddressText.setText(ipAddress);
+            serverButtonsLayout.addView(foundServerBtn);
+        }
     }
 }
