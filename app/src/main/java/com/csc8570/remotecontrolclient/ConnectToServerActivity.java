058e2b210d6388bb0e3cdc8393dc9163003b3788
Tom <redsoxfantom@gmail.com>
2016-03-14 22:14:43 -0400

    Got references to layout ids

--- app/src/main/java/com/csc8570/remotecontrolclient/ConnectToServerActivity.java
+++ app/src/main/java/com/csc8570/remotecontrolclient/ConnectToServerActivity.java
@@ -2,12 +2,20 @@ package com.csc8570.remotecontrolclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ConnectToServerActivity extends AppCompatActivity {
+
+    ProgressBar serverConnectionProgress;
+    TextView serverConnectionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_to_server);
+
+        serverConnectionProgress = (ProgressBar)findViewById(R.id.ServerConnectionStatusBar);
+        serverConnectionText = (TextView)findViewById(R.id.ServerConnectionStatusText);
     }
 }
