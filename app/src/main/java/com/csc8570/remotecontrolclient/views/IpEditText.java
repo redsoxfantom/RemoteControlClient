8c40700318a3f348c850c5d99d3899c74172f48c
Tom <redsoxfantom@gmail.com>
2016-03-15 22:12:27 -0400

    Work on getting ip text editor working

--- app/src/main/java/com/csc8570/remotecontrolclient/views/IpEditText.java
+++ app/src/main/java/com/csc8570/remotecontrolclient/views/IpEditText.java
@@ -2,10 +2,13 @@ package com.csc8570.remotecontrolclient.views;
 
 import android.content.Context;
 import android.graphics.Rect;
+import android.text.Editable;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.text.method.TransformationMethod;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -18,21 +21,20 @@ public class IpEditText extends EditText
 {
     public IpEditText(Context context) {
         super(context);
-        setInputType(InputType.TYPE_CLASS_NUMBER);
-        setTransformationMethod(new IpAddressTransformation());
-
+        setInputType(InputType.TYPE_CLASS_PHONE);
+        addTextChangedListener(new IpAddressTextWatcher(this));
     }
 
     public IpEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setInputType(InputType.TYPE_CLASS_NUMBER);
-        setTransformationMethod(new IpAddressTransformation());
+        setInputType(InputType.TYPE_CLASS_PHONE);
+        addTextChangedListener(new IpAddressTextWatcher(this));
     }
 
     public IpEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setInputType(InputType.TYPE_CLASS_NUMBER);
-        setTransformationMethod(new IpAddressTransformation());
+        setInputType(InputType.TYPE_CLASS_PHONE);
+        addTextChangedListener(new IpAddressTextWatcher(this));
     }
 
     public String getAddress()
@@ -42,21 +44,53 @@ public class IpEditText extends EditText
     }
 
     // Handles displaying a well formed IP address to the user
-    private class IpAddressTransformation implements TransformationMethod
+    private class IpAddressTextWatcher implements TextWatcher
     {
-        @Override
-        public CharSequence getTransformation(CharSequence source, View view) {
-            CharSequence[] sequences = new CharSequence[4];
-            sequences[0] = getSubSequence(source,0,3);
-            sequences[1] = getSubSequence(source,3,6);
-            sequences[2] = getSubSequence(source,6,9);
-            sequences[3] = getSubSequence(source,9,12);
+        private IpEditText textView;
 
-            return TextUtils.join(".",sequences);
+        public IpAddressTextWatcher(IpEditText textView)
+        {
+            this.textView = textView;
         }
 
-        // Returns a subsequence from a char sequence, or the max characters that could be extraced
-        private CharSequence getSubSequence(CharSequence input, int start, int end)
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // do nothing
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //do nothing
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            textView.removeTextChangedListener(this);
+            int selectionPos = textView.getSelectionEnd();
+
+            String ip = convertToIP(s.toString());
+            s.clear();
+            s.append(ip);
+
+            textView.setSelection(selectionPos);
+            textView.addTextChangedListener(this);
+        }
+
+        // Takes a string and inserts periods where necessary
+        private String convertToIP(String input)
+        {
+            input = input.replace(".","");
+            String retString = String.format("%s.%s.%s.%s",
+                    getSubSequence(input,0,3),
+                    getSubSequence(input,3,6),
+                    getSubSequence(input,6,9),
+                    getSubSequence(input,9,12));
+
+            return retString;
+        }
+
+        // Returns a subsequence from a char sequence, or the max characters that could be extracted
+        private String getSubSequence(String input, int start, int end)
         {
             if(start > input.length())
             {
@@ -64,14 +98,9 @@ public class IpEditText extends EditText
             }
             if(end > input.length())
             {
-                return input.subSequence(start,input.length());
+                return input.substring(start);
             }
-            return input.subSequence(start,end);
-        }
-
-        @Override
-        public void onFocusChanged(View view, CharSequence sourceText, boolean focused, int direction, Rect previouslyFocusedRect) {
-
+            return input.substring(start,end);
         }
     }
 }
