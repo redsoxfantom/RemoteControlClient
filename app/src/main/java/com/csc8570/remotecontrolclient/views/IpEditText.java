92d903af0d2c9c4c07f593671156f584081f18cd
Tom <redsoxfantom@gmail.com>
2016-03-16 19:38:03 -0400

    Work on ip address page

--- app/src/main/java/com/csc8570/remotecontrolclient/views/IpEditText.java
+++ app/src/main/java/com/csc8570/remotecontrolclient/views/IpEditText.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.net.InetAddress;
 
@@ -22,13 +23,57 @@ public class IpEditText extends EditText
 {
     public IpEditText(Context context) {
         super(context);
+        addTextChangedListener(new IpInputWatcher(this));
     }
 
     public IpEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        addTextChangedListener(new IpInputWatcher(this));
     }
 
     public IpEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        addTextChangedListener(new IpInputWatcher(this));
+    }
+
+    private class IpInputWatcher implements TextWatcher
+    {
+        private IpEditText view;
+
+        public IpInputWatcher(IpEditText view)
+        {
+            this.view = view;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+        {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+            view.removeTextChangedListener(this);
+
+            String currentText = s.toString();
+            if(currentText.length() > 3)
+            {
+                int currPos = view.getSelectionEnd();
+
+                s.clear();
+                currentText = currentText.substring(0,3);
+                s.append(currentText);
+                view.setSelection(3);
+            }
+
+            view.addTextChangedListener(this);
+        }
     }
 }
