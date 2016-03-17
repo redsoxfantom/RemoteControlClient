64e60f728fefb5088f161a304ee8d3147e375723
Tom <redsoxfantom@gmail.com>
2016-03-16 21:32:51 -0400

    Move the cursor to the next edit box when the user types in three characters

--- app/src/main/java/com/csc8570/remotecontrolclient/views/IpEditText.java
+++ app/src/main/java/com/csc8570/remotecontrolclient/views/IpEditText.java
@@ -1,6 +1,7 @@
 package com.csc8570.remotecontrolclient.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Rect;
 import android.text.Editable;
 import android.text.InputType;
@@ -10,9 +11,12 @@ import android.text.method.TransformationMethod;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.csc8570.remotecontrolclient.R;
 
 import java.net.InetAddress;
 
@@ -21,6 +25,8 @@ import java.net.InetAddress;
  */
 public class IpEditText extends EditText
 {
+    int nextBoxId;
+
     public IpEditText(Context context) {
         super(context);
         addTextChangedListener(new IpInputWatcher(this));
@@ -28,21 +34,42 @@ public class IpEditText extends EditText
 
     public IpEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
+        nextBoxId = getNextBoxId(context,attrs);
         addTextChangedListener(new IpInputWatcher(this));
     }
 
     public IpEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        nextBoxId = getNextBoxId(context,attrs);
         addTextChangedListener(new IpInputWatcher(this));
+    }
+
+    //  Get the resource Id of the next box
+    private int getNextBoxId(Context ctx, AttributeSet attrs)
+    {
+        TypedArray arry = ctx.obtainStyledAttributes(attrs, R.styleable.IpEditText, 0, 0);
+        int resourceId = -1;
+
+        try {
+            int idx = arry.getIndex(R.styleable.IpEditText_nextBox);
+            resourceId = arry.getResourceId(idx, -1);
+        }
+        finally {
+            arry.recycle();
+        }
+
+        return resourceId;
     }
 
     private class IpInputWatcher implements TextWatcher
     {
         private IpEditText view;
+        private IpEditText nextBox;
 
         public IpInputWatcher(IpEditText view)
         {
             this.view = view;
+            nextBox = null;
         }
 
         @Override
@@ -61,19 +88,29 @@ public class IpEditText extends EditText
         public void afterTextChanged(Editable s)
         {
             view.removeTextChangedListener(this);
+            getNextBox();
 
             String currentText = s.toString();
-            if(currentText.length() > 3)
+            if(currentText.length() >= 3)
             {
                 int currPos = view.getSelectionEnd();
 
                 s.clear();
                 currentText = currentText.substring(0,3);
                 s.append(currentText);
-                view.setSelection(3);
+
+                nextBox.requestFocus();
             }
 
             view.addTextChangedListener(this);
+        }
+
+        // Get the reference to the next box
+        private void getNextBox() {
+            if (nextBox == null) {
+                nextBox = (IpEditText) ((View) view.getParent()).findViewById(nextBoxId);
+
+            }
         }
     }
 }
